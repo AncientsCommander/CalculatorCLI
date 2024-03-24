db7f5e6ed8630906c4738b959073090797802225
Ancient <atlantis159@mail.ru>
2024-03-24 23:04:00 +0500

    Add sqrt and percent

--- CalculatorCLI/Program.cs
+++ CalculatorCLI/Program.cs
@@ -2,15 +2,16 @@
 using System.Text.RegularExpressions;
 
 double result;
+const string sqrtOperator = "sqrt";
 
 while (true)
 {
     Console.WriteLine("Введите формулу");
     var input = Console.ReadLine()?.Replace(" ", "");
-    
-    var isDigits = input is not null && Regex.IsMatch(input, @"^[\d+\-*/()%]+$");
 
-    if (!isDigits)
+    var isDigits = input is not null && Regex.IsMatch(input, @"^(sqrt|\d+|\+|-|\*|/|%|\(|\)|%|\.|,)+$");
+
+    if (!isDigits || input is null)
     {
         Console.WriteLine("Формула содержит недопустимые символы. Проверьте формулу и повторите");
         continue;
@@ -18,9 +19,17 @@ while (true)
 
     try
     {
-        result = double.Parse(new DataTable().Compute(input, string.Empty).ToString()!);
-        
+        var isSqrt = input.Contains(sqrtOperator);
+
+        if (isSqrt) input = input.Replace(sqrtOperator, "");
+        if (input.Contains('%')) input = input.Replace("%", "*0.01");
+
+        result = double.Parse(new DataTable().Compute(input.Replace(',', '.'), string.Empty).ToString()!);
+
         if (double.IsInfinity(result)) throw new ArgumentException("На 0 делить нельзя. Проверьте формулу и повторите");
+
+        if (isSqrt) result = Math.Sqrt(result);
+        if (result is double.NaN) throw new Exception();
     }
     catch (ArgumentException exception)
     {
